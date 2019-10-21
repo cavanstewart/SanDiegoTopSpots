cbd1bceee0de53030d4ce95984d81d4c4213a46d
Cavan Stewart <cavanwstewart@gmail.com>
2019-10-21 10:24:17 -0700

    Completed San Diego Top Spots table with location links

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
 $(document).ready(function() {
-// write your code here
-
+  // write your code here
+  $.getJSON("data.json", function(data) {
+    for (i = 0; i < data.length; i++) {
+      $("#sd-table").append(
+        "<tr>" +
+          "<td>" + data[i]["name"] + "</td>" +
+          "<td>" + data[i]["description"] + "</td>" +
+          "<td>" + "<button href=\"" + "https://www.google.com/maps?q=" + data[i]["location"][0] + "," + data[i]["location"][1] + "\"" + " class=\"loc-button\">Open in Google Maps</button>" + "</td>" +
+        "</tr>"
+      );
+    }
+  });
 });
